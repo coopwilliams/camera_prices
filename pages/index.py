c5430fc0e30d3a952fb6d2f2e92ca331d57b4845
Cooper Williams <strangelycutlemon@gmail.com>
2019-09-26 11:45:35 -0400

    update index.py and process.py

--- pages/index.py
+++ pages/index.py
@@ -10,17 +10,17 @@ from app import app
 """
 https://dash-bootstrap-components.opensource.faculty.ai/l/components/layout
 
-Layout in Bootstrap is controlled using the grid system. The Bootstrap grid has 
+Layout in Bootstrap is controlled using the grid system. The Bootstrap grid has
 twelve columns.
 
-There are three main layout components in dash-bootstrap-components: Container, 
+There are three main layout components in dash-bootstrap-components: Container,
 Row, and Col.
 
 The layout of your app should be built as a series of rows of columns.
 
-We set md=4 indicating that on a 'medium' sized or larger screen each column 
-should take up a third of the width. Since we don't specify behaviour on 
-smaller size screens Bootstrap will allow the rows to wrap so as not to squash 
+We set md=4 indicating that on a 'medium' sized or larger screen each column
+should take up a third of the width. Since we don't specify behaviour on
+smaller size screens Bootstrap will allow the rows to wrap so as not to squash
 the content.
 """
 
@@ -28,18 +28,16 @@ column1 = dbc.Col(
     [
         dcc.Markdown(
             """
-        
-            ## Value Proposition
 
-            Emphasize how the app will benefit users. Don't emphasize the underlying technology.
+            ## Understanding the Cost of Digital Camera Features
 
-            ✅ RUN is a running app that adapts to your fitness levels and designs personalized workouts to help you improve your running.
-
-            ❌ RUN is the only intelligent running app that uses sophisticated deep neural net machine learning to make your run smarter because we believe in ML driven workouts.
+            This tool is an interactive predictive built using the features of over 1,000 digital cameras released from 1994 to 2007 (the year the iPhone was released).
+            With it, you can estimate the cost of a camera from that time period with any features you specify.
+            While it won't help you choose a digital camera today, it does illustrate which features were most costly during the heyday of digital cameras.
 
             """
         ),
-        dcc.Link(dbc.Button('Call To Action', color='primary'), href='/predictions')
+        dcc.Link(dbc.Button('Try it out', color='primary'), href='/predictions')
     ],
     md=4,
 )
--- pages/process.py
+++ pages/process.py
@@ -10,8 +10,17 @@ column1 = dbc.Col(
     [
         dcc.Markdown(
             """
-        
-            ## Process
+
+            ## How this tool was created
+
+            This predictive model was made using the Python library scikit-learn, and the interactive web app uses Plotly Dash and Heroku.
+
+            The statistical model is a random forest regressor, a machine learning technique that understands data using a "wisdom of the crowd" approach. This means that it combines thousands of random samples from the data to create a generally accurate consensus of the target (in this case, camera prices).
+            I chose this algorithm because it has a low risk of overfitting and is straightforward to use.
+            There are a couple downsides. A random forest regressor cannot extrapolate to data outside the range of values it has seen, so the interactive tool can't imagine the price of a hypothetical 11MP camera in 1999.
+            This model is also very difficult to interpret, in the sense that changes in values don't have a straightforward effect on the price like you might see from a linear regression model.
+            So the best we can do to estimate those effects is to play with the sliders!
+
 
 
             """
